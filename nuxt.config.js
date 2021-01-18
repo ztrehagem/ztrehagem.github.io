04a237a6948cb1e5864f2c619fdddad5e2837c8a
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2021-01-19 02:16:40 +0900

    init nuxt app

---
+++ nuxt.config.js
@@ -0,0 +1,34 @@
+export default {
+  target: 'static',
+
+  head: {
+    title: 'ztrehagem.github.io',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/tailwindcss',
+  ],
+
+  modules: [],
+
+  srcDir: 'src',
+
+  build: {},
+}
