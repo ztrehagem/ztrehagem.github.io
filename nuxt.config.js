88bf1daf146ebeb02b1d2cae81245ac1b7c7a025
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2021-01-20 00:02:37 +0900

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,31 @@
+const publicPath = process.env.PUBLIC_PATH ?? 'https://ztrehagem.dev/'
+const title = 'ztrehagem.dev'
+const description = 'a portfolio'
+
 export default {
   target: 'static',
 
   head: {
-    title: 'ztrehagem.github.io',
+    title,
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: description, content: '' },
+      { property: 'og:title', content: title },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: publicPath },
+      { property: 'og:image', content: 'https://github.com/ztrehagem.png' },
+      { property: 'og:locale', content: 'ja_JP' },
+      { property: 'og:site_name', content: title },
+      { property: 'og:description', content: description },
+      { name: 'twitter:card', content: 'summary' },
+      { name: 'twitter:site', content: '@ztrehagem' },
+      { name: 'theme-color', content: '#3C3C3C' },
+      { name: 'msapplication-navbutton-color', content: '#3C3C3C' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: '#3C3C3C' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
